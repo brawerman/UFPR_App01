e2382ca7d65dc9f839b0d9a8c88d026043c57b93
“brawerman” <brawerman@gmail.com>
2021-05-16 19:00:42 -0300

    Configuracao do servidor e rotas.

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//Informa qual biblioteca usar para o parsing do conteúdo
+//True = qs (suporta nested objects)
+//False = querystring
+app.use(express.urlencoded({ extended: false }));
+//Parser para json
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({ msg: "Olá" });
+});
+
+app.get("/users", (req, res) => {
+	const user = [{ name: "Bruno" }, { name: "Carla" }];
+	res.status(200).json({ msg: "Listagem de usuários.", user });
+});
+
+app.post("/users/newUser", (req, res) => {
+	const user = req.body.user;
+	if (user != undefined)
+		res.status(200).json({ msg: "Usuário adicionado com sucesso" });
+	else res.status(400).json({ msg: "Preencha o campo usuário" });
+});
+
+app.listen(3333, () => {
+	console.log("Servidor na porta 3333");
+});
